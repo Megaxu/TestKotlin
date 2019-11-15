8a64cf3b0b306fe7338658cbee936dfd0ed215c6
Mike <mixanik009@gmail.com>
2019-11-15 23:32:11 +0300

    Creat class Cat

---
+++ src/Cat.kt
@@ -0,0 +1,29 @@
+class Cat(val name: String, private val initialWeight: Double) {
+
+    private var weight: Double = initialWeight
+
+    fun feed(food: Double) {
+        this.weight += food
+        println("Котик стал толще на $food кг")
+    }
+
+    fun goToToilet() {
+        this.weight -= this.weight / 10
+    }
+
+    fun getStatus() {
+        if(this.weight < initialWeight){
+            println("Котик голоден, покорми его скорее")
+        }
+        else if(this.weight >= initialWeight && this.weight <= initialWeight * 2) {
+            println("Котик покушал и чувствует себя хорошо")
+        }
+        else{
+            println("Котик смльно переел, ему нужно отдохнуть")
+        }
+    }
+
+    fun getWeight() {
+        println("Котик весит ${this.weight} кг")
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    
+    val myCat: Cat = Cat("Митя", 3.2)
+    myCat.feed(.2)
+    myCat.getStatus()
+    myCat.goToToilet()
+    myCat.getStatus()
+
 }
